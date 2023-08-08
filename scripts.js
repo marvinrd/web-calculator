dd3eed01e9325206b8b3000941e96b42c35bc6a0
Marvin Delos Santos <marvin.dlsnts@gmail.com>
2023-08-08 17:12:04 +0200

    Add js simple functions Add placeholder operate function@

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//references to html doc
+const display = document.querySelector('#displayOutput')
+
+//initialize values
+firstNumber = 0;
+secondNumber = 0;
+operator = '+';
+
+
+//simple functions
+const add = (a,b) => {return a+b};
+const subtract = (a,b) => {return a-b};
+const multiply = (a,b) => {return a*b};
+const divide = (a,b) => {return a/b};
+
+//new function operate
+const operate = function (firstNumber, operator, secondNumber) {
+    
+};
